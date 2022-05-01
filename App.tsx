20b40a8922bda11ee76237ff700364ff1a16771e
Noé <noe.philippe@bob-le-developpeur.com>
2022-05-01 11:30:42 +0200

    Ajout du persist store

--- App.tsx
+++ App.tsx
@@ -3,54 +3,59 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { appStyle } from './appStyle';
 import { HomePage, SearchPage } from './src/pages';
 import store from './store';
 
 export default function App(): JSX.Element {
   const Tabs = createBottomTabNavigator();
+  let persistor = persistStore(store);
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Tabs.Navigator
-          initialRouteName='Home'
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, size, color }) => {
-              let iconName = 'home';
-              if (route.name == 'Home') {
-                iconName = focused ? 'home' : 'home-outline';
-                color = focused ? 'white' : 'grey';
-              } else if (route.name == 'Search') {
-                iconName = focused ? 'search' : 'search-outline';
-                color = focused ? 'white' : 'grey';
-              }
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarStyle: appStyle.tab,
-            tabBarActiveTintColor: 'white',
-          })}
-        >
-          <Tabs.Screen
-            name='Home'
-            component={HomePage}
-            options={{
-              title: 'Bibliothèque',
-              headerShown: false,
-            }}
-            initialParams={{ goBack: null }}
-          />
-          <Tabs.Screen
-            name='Search'
-            component={SearchPage}
-            options={{
-              title: 'Rechercher',
-              headerShown: false,
-            }}
-            initialParams={{ goBack: null }}
-          />
-        </Tabs.Navigator>
-      </NavigationContainer>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Tabs.Navigator
+            initialRouteName='Home'
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, size, color }) => {
+                let iconName = 'home';
+                if (route.name == 'Home') {
+                  iconName = focused ? 'home' : 'home-outline';
+                  color = focused ? 'white' : 'grey';
+                } else if (route.name == 'Search') {
+                  iconName = focused ? 'search' : 'search-outline';
+                  color = focused ? 'white' : 'grey';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarStyle: appStyle.tab,
+              tabBarActiveTintColor: 'white',
+            })}
+          >
+            <Tabs.Screen
+              name='Home'
+              component={HomePage}
+              options={{
+                title: 'Bibliothèque',
+                headerShown: false,
+              }}
+              initialParams={{ goBack: null }}
+            />
+            <Tabs.Screen
+              name='Search'
+              component={SearchPage}
+              options={{
+                title: 'Rechercher',
+                headerShown: false,
+              }}
+              initialParams={{ goBack: null }}
+            />
+          </Tabs.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- store.js
+++ store.js
@@ -1,10 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 import songDetailReducer from './src/redux/songDetailSlice';
 import songReducer from './src/redux/songSlice';
 
+const persistConfig = {
+  key: 'songSeeker',
+  storage: AsyncStorage,
+};
+
+const reducers = combineReducers({
+  songDetail: songDetailReducer,
+  song: songReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-  reducer: {
-    songDetail: songDetailReducer,
-    song: songReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
